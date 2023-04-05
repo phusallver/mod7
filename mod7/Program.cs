1d9fc0f5d2f7bb90bf69c7d4936441e92d598484
unknown <gonhgyhjj@student.telkomuniversity.ac.id>
2023-04-05 17:28:44 +0700

    menambahkan class TeamMembers1302210124

--- mod7/Program.cs
+++ mod7/Program.cs
@@ -1,4 +1,5 @@
 ﻿
+using mod7;
 using System;
 internal class Program
 {
@@ -6,5 +7,9 @@ internal class Program
     {
         DataMahasiswa1302210124 ryan = new DataMahasiswa1302210124();
         ryan.ReadJSON();
+
+        Console.WriteLine("\n=======================================\n");
+        TeamMembers1302210059 anggota = new TeamMembers1302210059();
+        anggota.ReadJSON();
     }
 }
